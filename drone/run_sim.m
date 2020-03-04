d224b92b4e0878207a8d539f87561b362e83814d
henry-esl <19231865@sun.ac.za>
2020-03-04 18:23:31 +0200

    implemented crude disturbances

--- drone/run_sim.m
+++ drone/run_sim.m
@@ -2,7 +2,7 @@
 % Duration of simulation
 sim_time = 10;
 % Step inputs
-pos_e_step = [10,2,3];
+pos_e_step = [0,2,0];
 yaw_step = 0.00;
 
 %% Setup
@@ -12,6 +12,18 @@ setup_simulation;
 
 %% Sensor Noise
 sensor_noise = 0;
+
+%% Disturbance
+disturbance_on_off = 1;
+force_disturbance = 0;
+moment_disturbance = 1;
+
+% Activate disturbance rejection
+ADRC = 0;
+
+%% Neural Network Effective Time Delay
+ROS2_delay=1/sim_freq;
+
 
 %% Run simulation
 sim('px4_mc_sim');
